0d72ccb230cb1bc15e82a0567e5991580881a5cb
Fail <failkozyrev005@gmail.com>
2024-04-06 13:13:47 +0400

    initial commit

---
+++ Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr2
+{
+    public class Circle
+    {
+        public const double PI = Math.PI;
+        public double Radius { get; set; }
+
+        public double CalculatePerimeter()
+        {
+            return 2 * PI * Radius;
+        }
+
+        public double CalculateArea()
+        {
+            return PI * Radius * Radius;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace pr2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
